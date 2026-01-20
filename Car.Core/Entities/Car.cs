4b3f3b9ac0f5b7ea4c0d1c47bcee06ed1ba40926
veroncav <veronika1999.v@gmail.com>
2026-01-20 10:54:32 +0200

    Initial project with complete setup: Core, Data, ApplicationServices, Web, Car entity, repository, service, controller

---
+++ Car.Core/Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Car.Core.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }  // Марка
+        public string Model { get; set; } // Модель
+        public int Year { get; set; }     // Год выпуска
+        public string Color { get; set; } // Цвет
+        public decimal Price { get; set; } // Цена
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  // Создано
+        public DateTime ModifiedAt { get; set; } = DateTime.UtcNow; // Изменено
+    }
+}
