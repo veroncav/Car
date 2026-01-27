0ccc8120fbaeb45fa47b207c6c30083a4e0ec1a6
veroncav <veronika1999.v@gmail.com>
2026-01-27 09:29:02 +0200

    Car Web

---
+++ Car.Web/Program.cs
@@ -0,0 +1,31 @@
+using Car.ApplicationServices.Services;
+using Car.Core.Interfaces;
+using Car.Data;
+using Car.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<CarService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Cars}/{action=Index}/{id?}");
+
+app.Run();
